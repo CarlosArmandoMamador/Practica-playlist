23b8daeaa91638bb2cca144d5cede27cd91d6207
CarlosArmandoMamador <owonerosan05@gmail.com>
2024-03-08 00:49:07 -0600

    hicimos una pequeña playlist en java Para el profe alfonso

---
+++ src/main/java/com/mycompany/playlist/Playlist.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.playlist;
+
+/**
+ *
+ * @author DELL
+ */
+public class Playlist {
+
+    public static void main(String[] args) {
+        Musicas Pl1=new Musicas("El Paso Del Gigante","Cumbias Mexicanas","1987",3);
+        Pl1.ShowPlaylist();
+    }
+}
+ 
